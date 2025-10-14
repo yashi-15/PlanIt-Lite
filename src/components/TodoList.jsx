6a739557dc7250e50f8c200ac572e21ec59c3bdb
yashi-15 <sharmayashi270@gmail.com>
2025-10-15 00:09:46 +0530

    ui polish

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,7 +4,15 @@ import TodoItem from "./TodoItem";
 const TodoList = ({ todos, deleteTodo, setEditMode, markCompleteTodo }) => {
     return (
         <div className="basis-2/3 mx-auto max-w-xl overflow-y-scroll modern-thin">
-            <h1 className="text-violet-300 text-2xl mb-4 text-center">Your Tasks</h1>
+            <h1 className="text-violet-300 text-2xl mb-4 text-center sticky top-0 bg-zinc-900">Your Tasks</h1>
+            {/* <div>
+                <select name="cars" id="cars">
+                    <option value="volvo">Volvo</option>
+                    <option value="saab">Saab</option>
+                    <option value="mercedes">Mercedes</option>
+                    <option value="audi">Audi</option>
+                </select>
+            </div> */}
             <div className="flex flex-col gap-2 divide-y divide-gray-600">
                 {todos.map((todo) => {
                     return <TodoItem key={todo.id} data={todo} setEditMode={setEditMode} deleteTodo={deleteTodo} markCompleteTodo={markCompleteTodo} />;
